8cc477915e1b54cc3987a7e7cc52ee8714d0ea00
nrgy13 <Sanctumizm@gmail.com>
2025-05-16 23:19:25 +0300

    Update Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,13 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    domains: ['ytbtznozmjlifztitlas.supabase.co'],
+    unoptimized: true
+  },
   webpack: (config, { dev }) => {
     // Disable filesystem cache to prevent ENOENT errors
     config.cache = false;
     return config;
-  },
-  images: {
-    domains: ['ytbtznozmjlifztitlas.supabase.co'],
-    unoptimized: true
   }
 };
 
