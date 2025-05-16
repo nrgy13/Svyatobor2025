b116b97058d97a6684a5503f29d48e60a983a743
nrgy13 <Sanctumizm@gmail.com>
2025-05-16 23:00:23 +0300

    Fix strokeLinejoin typo in ServicesSection.tsx

--- components/ServicesSection.tsx
+++ components/ServicesSection.tsx
@@ -140,8 +140,7 @@ export default function ServicesSection() {
             aria-label="Предыдущая услуга"
           >
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLin
-ejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
+              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 19l-7-7 7-7" />
             </svg>
           </button>
           <button 
