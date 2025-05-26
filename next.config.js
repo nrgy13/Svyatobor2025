4d8f5ccf61a7b80d11b3ccade3629c273fe5efd7
nrgy13 <Sanctumizm@gmail.com>
2025-05-26 16:25:45 +0300

    Fix missing images configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    domains: ['images.pexels.com'],
+    domains: ['images.pexels.com', 'ytbtznozmjlifztitlas.supabase.co'],
+    unoptimized: true
   },
-}
+  webpack: (config, { dev }) => {
+    // Disable filesystem cache to prevent ENOENT errors
+    config.cache = false;
+    return config;
+  }
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
