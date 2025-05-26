2ea6d4477a162bbc0ac42ebf0af4be544e57fa42
nrgy13 <Sanctumizm@gmail.com>
2025-05-26 16:24:30 +0300

    Add image constants

--- next.config.js
+++ next.config.js
@@ -1,15 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    domains: ['ytbtznozmjlifztitlas.supabase.co'],
-    unoptimized: true
+    domains: ['images.pexels.com'],
   },
-  webpack: (config, { dev }) => {
-    // Disable filesystem cache to prevent ENOENT errors
-    config.cache = false;
-    return config;
-  }
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
